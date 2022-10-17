e1e800c3217840ad52dd985ef4780aa3f99277bc
kanta nakagawa <kan0401naka@gmail.com>
2022-10-17 18:59:52 +0900

    論文投稿アプリ課題完了

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.main')
 @section('title', '論文修正')
 @section('content')
-    <h1>論文修正</h1>
+    <h1>投稿論文編集</h1>
+
+    <form action="{{ route('articles.update', $article) }}" method="post">
+        @csrf
+        @method('patch')
+        <div>
+            <label for="title">論文タイトル:</label>
+            <input type="text" name="title" id="title" value="{{ old("title", $article->title) }}">
+        </div>
+        <div>
+            <label for="body">本文:</label>
+            <textarea name="body" id="body" cols="30" rows="10">{{ old("body", $article->body) }}</textarea>
+        </div>
+        <div>
+            <input type="submit" value="更新">
+        </div>
+    </form>
+
 @endsection
